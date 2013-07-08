1f55822d4bb7cb6073ff87fcdfdc7247e2cb8612
Ahmed Boussadia <ahmed.boussadia@gmail.com>
2013-07-08 11:28:12 +0200

    Adding get method to retrieve prospects list

--- invitations/views.py
+++ invitations/views.py
@@ -4,8 +4,25 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.conf import settings
 
-from django.contrib.auth.decorators import user_passes_test
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from invitations.models import Prospect
+from invitations.serializer import ProspectSerializer
 
 
 def index(request):
 	return render(request, 'invitations/index.html', {'DEBUG': settings.DEBUG})
+
+class ProspectsAPIView(APIView):
+	"""
+		Prospect api view.
+	"""
+
+	def get(self, request):
+		"""
+			Getting all prospect lists.
+		"""
+		prospects = Prospect.objects.all()
+		serializer = ProspectSerializer(prospects, many = True)
+		return Response(serializer.data)
